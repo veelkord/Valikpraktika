cc522d1b6c264f2dc2f513a2cf9412d07d893cc2
veelkord <79205178+veelkord@users.noreply.github.com>
2022-11-01 09:14:50 +0200

    Revert "Revert "Dev""

--- development/api/src/app.ts
+++ development/api/src/app.ts
@@ -51,21 +51,15 @@ app.patch("/users/:id", checkAlphabet, userController.updateUserById);
 //Schedule endpoints
 
 app.get("/schedule", scheduleController.getEntireSchedule);
+app.post("/schedule", scheduleController.createSchedule);
 
 // Lecturer endpoints
 app.get("/lecturers", lecturerController.getAllLecturersById);
 app.get("/lecturers/activeSubjects", lecturerController.getLecturersSubjects);
 app.get("/lecturers/:id", lecturerController.getLecturerById);
 app.post("/lecturers", checkAlphabet, lecturerController.addLecturer);
-app.delete(
-  "/lecturers/:id",
-  lecturerController.deleteLecturerWhenNoSubjectsById
-);
-app.patch(
-  "/lecturers/:id",
-  checkAlphabet,
-  lecturerController.updateLecturerById
-);
+app.delete("/lecturers/:id", lecturerController.deleteLecturerWhenNoSubjectsById);
+app.patch("/lecturers/:id", checkAlphabet, lecturerController.updateLecturerById);
 
 // Subjects endpoints
 app.get("/subjects", subjectController.getAllSubjects);
@@ -97,7 +91,7 @@ app.get("/homeworks/:id", homeworkController.getHomeworkById);
 app.post("/homeworks", homeworkController.addHomework);
 app.delete("/homeworks/:id", homeworkController.deleteHomework);
 app.patch("/homeworks/:id", homeworkController.updateHomeworkById);
-app.get("/homeworksbycode/:code", homeworkController.getHomeworkByCode);
+app.get("/homeworksbycode/:code/:actualDate", homeworkController.getHomeworkByCode);
 
 
 export default app;
